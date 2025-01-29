d715c4aad5bcfd0ba02018538624bf6c3d20fc48
JNewbins <151698594+JNewbins@users.noreply.github.com>
2025-01-29 17:42:28 -0600

    Added Fragment Component

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,16 +1,17 @@
 import { View, StyleSheet } from 'react-native';
 import { Link, Stack } from 'expo-router';
+import { Fragment } from 'react';
 
 export default function NotFoundScreen() {
   return (
-    <>
+    <Fragment>
       <Stack.Screen options={{ title: 'Oops! Not Found' }} />
       <View style={styles.container}>
         <Link href="/" style={styles.button}>
           Go back to Home screen!
         </Link>
       </View>
-    </>
+    </Fragment>
   );
 }
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,15 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { Fragment } from 'react';
 
 export default function RootLayout() {
   return (
-    <>
+    <Fragment>
        <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
     <StatusBar style="light" />
-    </>
+    </Fragment>
   );
 }
